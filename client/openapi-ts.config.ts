f119623c773defca7214f2f6482057e495a7df3c
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-10 21:45:37 +0100

    update openapi-ts config path

--- client/openapi-ts.config.ts
+++ client/openapi-ts.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     {
       name: '@hey-api/client-next',
       throwOnError: false,
-      runtimeConfigPath: '../../config/hey-api.ts',
+      runtimeConfigPath: '../../config/hey-api',
     },
     {
       name: 'zod',
