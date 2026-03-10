65b450e4df5ae04ef7da302ec84a3b91b3d1647b
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-10 22:38:11 +0100

    setup auth correctly

--- client/src/config/hey-api.ts
+++ client/src/config/hey-api.ts
@@ -1,6 +1,16 @@
-import { CreateClientConfig } from '@/lib/api-client/client';
+import { cookies } from 'next/headers';
+
+import { Auth, CreateClientConfig } from '@/lib/api-client/client';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
   baseUrl: process.env.API_BASE_URL,
+  auth: async (auth: Auth) => {
+    console.log('Security check:', auth);
+
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+
+    return token;
+  },
 });
