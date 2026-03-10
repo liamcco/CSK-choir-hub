17d5bd004e3c7ddf83d518e7e0b02249580cdc36
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-10 21:51:03 +0100

    udpate client config

--- client/src/config/hey-api.ts
+++ client/src/config/hey-api.ts
@@ -2,5 +2,5 @@ import { CreateClientConfig } from '@/lib/api-client/client';
 
 export const createClientConfig: CreateClientConfig = (config) => ({
   ...config,
-  // baseUrl: "https://example.com",
+  baseUrl: process.env.API_BASE_URL,
 });
