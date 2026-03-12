ac79bf6b08eada862e1d9a493422e7e17c0a0964
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-12 10:55:44 +0100

    remove loading

--- client/src/app/events/create/page.tsx
+++ client/src/app/events/create/page.tsx
@@ -1,10 +1,15 @@
+import { Suspense } from 'react';
+
 import { CreateEventForm } from '@/app/events/create/create-event-form';
+import { CreateEventFormSkeleton } from '@/app/events/create/create-event-form-skeleton';
 
 export default function Page() {
   return (
     <div className="flex h-full w-full items-center justify-center px-8 sm:px-6">
       <div className="w-full max-w-sm">
-        <CreateEventForm />
+        <Suspense fallback={<CreateEventFormSkeleton />}>
+          <CreateEventForm />
+        </Suspense>
       </div>
     </div>
   );
