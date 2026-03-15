f32c95c80b7b054c4f3f128f479d9cabadc2606b
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-15 23:27:44 +0100

    remove password hash

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { dash } from '@better-auth/infra';
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { organization } from 'better-auth/plugins';
 
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from '@/db/prisma';
@@ -10,7 +11,7 @@ const BASE_URL = process.env.BETTER_AUTH_URL || 'http://localhost:5050/';
 export const auth = betterAuth({
   baseURL: BASE_URL,
   emailAndPassword: { enabled: true },
-  plugins: [dash()],
+  plugins: [dash(), organization()],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
