234a98efb80c8b72e4a19bcd49d7224ac845f581
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-16 00:04:06 +0100

    prepare for clientwork

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -3,6 +3,7 @@ import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { organization } from 'better-auth/plugins';
 import { admin } from 'better-auth/plugins';
+import { openAPI } from 'better-auth/plugins';
 
 // If your Prisma file is located elsewhere, you can change the path
 import { prisma } from '@/db/prisma';
@@ -10,9 +11,11 @@ import { prisma } from '@/db/prisma';
 const BASE_URL = process.env.BETTER_AUTH_URL || 'http://localhost:5050/';
 
 export const auth = betterAuth({
+  appName: 'CSK Choir Hub',
   baseURL: BASE_URL,
-  emailAndPassword: { enabled: true },
-  plugins: [dash(), organization(), admin()],
+  trustedOrigins: ['http://localhost:3000', 'https://csk-choir-hub.vercel.app'],
+  emailAndPassword: { enabled: true, disableSignUp: true },
+  plugins: [dash(), organization(), admin(), openAPI()],
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
