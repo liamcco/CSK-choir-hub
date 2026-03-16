b6e8691dfc35cee6b92441ee236d9a5e76fdca8d
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-16 22:10:30 +0100

    simplify loading

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,11 +1,3 @@
-'use client';
-
-import { Spinner } from '@/components/ui/spinner';
-
 export default function Loading() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Spinner />
-    </div>
-  );
+  return <div className="flex h-screen flex-col items-center justify-center">Loading...</div>;
 }
