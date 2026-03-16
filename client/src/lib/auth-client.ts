dcf99c050390f834fd494efde4c7310c4db97eef
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-17 00:24:09 +0100

    update config

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -4,6 +4,6 @@ import { passkeyClient } from "@better-auth/passkey/client"
 
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:5050",
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5050",
     plugins: [usernameClient(), organizationClient(), twoFactorClient(), emailOTPClient(), passkeyClient()],
 })
