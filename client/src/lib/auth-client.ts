61da3db98697ecc29349fb96cdf5c09aa32c04db
Liam <37960435+liamcco@users.noreply.github.com>
2026-03-17 00:35:55 +0100

    update config

--- client/src/lib/auth-client.ts
+++ client/src/lib/auth-client.ts
@@ -2,8 +2,10 @@ import { createAuthClient } from "better-auth/react"
 import { organizationClient, twoFactorClient, usernameClient, emailOTPClient } from "better-auth/client/plugins"
 import { passkeyClient } from "@better-auth/passkey/client"
 
+const BASE_URL = process.env.NEXT_PUBLIC_AUTH_BASE_URL || 'http://localhost:5050/api/auth';
+
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5050",
+    baseURL: BASE_URL,
     plugins: [usernameClient(), organizationClient(), twoFactorClient(), emailOTPClient(), passkeyClient()],
 })
